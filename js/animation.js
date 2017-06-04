a5620ff5a108505e83238577e59d92046e470fe7
iamstevendao <iamstevendao@users.noreply.github.com>
2017-06-04 11:54:04 +1000

    working on pages

--- js/animation.js
+++ js/animation.js
@@ -1,19 +1,2 @@
-$(document).ready(function () {
-	$(function () {
-		$("a[rel]").overlay({
-
-			mask: 'darkred',
-			effect: 'apple',
-
-			onBeforeLoad: function () {
-
-				// grab wrapper element inside content
-				var wrap = this.getOverlay().find(".contentWrap");
-
-				// load the page specified in the trigger
-				wrap.load(this.getTrigger().attr("href"));
-			}
-
-		});
-	});
+$(document).ready(function () {});
 });
