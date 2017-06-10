ce2f3eea0fb254e84fab5293105db25cb03a2bd0
iamstevendao <iamstevendao@users.noreply.github.com>
2017-06-11 05:10:03 +1000

    done

--- js/animation.js
+++ js/animation.js
@@ -1,2 +1,7 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+	$(".nav-button").on("click", openMenu);
+
+	function openMenu() {
+		$(".navi-bar ul").toggleClass("open");
+	}
 });
